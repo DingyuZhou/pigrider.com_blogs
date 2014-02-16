cd1c596d5f9f90918fd4a28699136d11d7736b46
pigrider.com <pigrider.com@gmail.com>
2014-02-15 20:58:41 -0500

    [Optimize]: Removed some weird logic in the timezone getting logic.

--- Gemfile
+++ Gemfile
@@ -15,7 +15,7 @@ end
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
-gem 'mysql2', '0.3.11'
+gem 'mysql2', '0.3.15'
 
 
 # Gems used only for assets and not required
--- app/assets/javascripts/common/getUserTimeZone.js
+++ app/assets/javascripts/common/getUserTimeZone.js
@@ -1,5 +1,17 @@
 (function() {
-  var tTime=new Date();
-  var nTimeZoneOffset=-tTime.getTimezoneOffset()/60;
-  document.cookie="userTimeZone="+nTimeZoneOffset;
+  // check if there is cookie "userTimeZone"
+  var iBeginPosition = document.cookie.indexOf(";userTimeZone=");
+  if (iBeginPosition == -1) {
+    iBeginPosition = document.cookie.indexOf("userTimeZone=");
+    if (iBeginPosition != 0) {
+      iBeginPosition = -1;
+    }
+  }
+  
+  if (iBeginPosition == -1) {
+    var tTime=new Date();
+    var nTimeZoneOffset=-tTime.getTimezoneOffset()/60;
+    document.cookie="userTimeZone="+nTimeZoneOffset;
+    window.location.reload();
+  }
 })();
